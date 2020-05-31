7e718d7f6e95a8bc00d83f71c4e0097416c143ef
Simon Fish <si@mon.fish>
2020-05-31 20:01:59 +0100

    Refactor styles in hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Img from "gatsby-image";
 import { graphql } from "gatsby";
-import styles from "./hero.module.scss";
 import usePortfolioImages from "../hooks/use-portfolio-images";
 import { useStyletron } from "styletron-react";
 
@@ -19,20 +18,86 @@ export default ({ data }) => {
         },
       })}
     >
-      <div className={styles.heroTextContainer}>
-        <h1 className={styles.heroHeadline}>{data.shortBio.shortBio}</h1>
+      <div
+        className={css({
+          minHeight: "86vh",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          "@media (min-width: 768px)": {
+            margin: "0 3rem",
+            width: "90%",
+          },
+        })}
+      >
+        <h1
+          className={css({
+            fontWeight: "bold",
+            gridColumn: 1,
+            fontSize: "3rem",
+            "@media (min-width: 768px)": {
+              fontSize: "5rem",
+            },
+          })}
+        >
+          {data.shortBio.shortBio}
+        </h1>
         <p className="lead">
-          University of Sheffield Computer Science graduate with a year of
-          industry experience. Experienced with{" "}
-          <span className="bg-light">Ruby</span>
+          University of Sheffield Computer Science graduate with a{" "}
+          <span
+            className={css({
+              borderBottom: ".2rem solid #047",
+              padding: ".1rem 0",
+            })}
+          >
+            year of industry experience
+          </span>
+          . Experienced with{" "}
+          <span
+            className={css({
+              borderBottom: ".2rem solid red",
+              padding: ".1rem 0",
+            })}
+          >
+            Ruby
+          </span>
+          ,{" "}
+          <span
+            className={css({
+              borderBottom: ".2rem solid aqua",
+              padding: ".1rem 0",
+            })}
+          >
+            React
+          </span>
+          , and more.
         </p>
       </div>
-      <div className={styles.imageBlock}>
+      <div>
         {images.slice(0, 3).map((image) => (
           <Img
             fluid={image.fluid}
             aspectRatio={image.fluid.aspectRatio}
-            className={styles.heroImage}
+            className={css({
+              transform: "rotateX(-15deg) rotateY(15deg)",
+              boxShadow: ".5em .5em 0.2em 0.2em rgba($gray-700, 0.4)",
+              marginBottom: "2em",
+              width: "auto",
+              height: "auto",
+              animationDuration: "1s",
+              animationTimingFunction: "ease-in-out",
+              animationName: {
+                from: {
+                  transform: "translateY(2em) rotateX(0) rotateY(0deg)",
+                  opacity: 0,
+                },
+                to: {
+                  transform: "translateY(0) rotateX(-15deg) rotateY(15deg)",
+                  opacity: 1,
+                },
+              },
+            })}
             imgStyle={{ objectFit: "contain" }}
           />
         ))}
