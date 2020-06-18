131da24cfcb1b1c74ad0187d6503ea8822cb8191
Simon Fish <si@mon.fish>
2020-06-18 13:45:33 +0100

    Revert to file url

--- src/components/helmet.js
+++ src/components/helmet.js
@@ -20,8 +20,8 @@ export default (props) => {
         }
         contentfulPerson {
           image {
-            fluid {
-              base64
+            file {
+              url
             }
           }
         }
@@ -41,14 +41,14 @@ export default (props) => {
       {/* {isBlogPost ? <meta property="og:type" content="article" /> : null} */}
       <meta property="og:title" content={data.site.siteMetadata.title} />
       <meta property="og:description" content={data.site.siteMetadata.description} />
-      <meta property="og:image" content={data.contentfulPerson.image.fluid.base64} />
+      <meta property="og:image" content={data.contentfulPerson.image.file.url} />
 
       {/* Twitter Card tags */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={data.contentfulSocialLink.link} />
       <meta name="twitter:title" content={data.site.siteMetadata.title} />
       <meta name="twitter:description" content={data.site.siteMetadata.description} />
-      <meta property="twitter:image" content={data.contentfulPerson.image.fluid.base64} />
+      <meta property="twitter:image" content={data.contentfulPerson.image.file.url} />
       <script type="application/ld+json">
         {`
         {
